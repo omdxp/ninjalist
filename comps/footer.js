f0ef8c5e688554a15bbd3c40eedf7a3b12107374
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-19 14:33:24 +0100

    adding links

---
+++ comps/footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+      Copyright &copy; {new Date().getFullYear()} - All rights reserved.
+    </div>
+  );
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,19 @@
+import Footer from "../comps/footer";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
+import Navbar from "../comps/navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
+      <Footer />
     </div>
   );
 }
