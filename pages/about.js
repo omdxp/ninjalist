47f82861966a69565875548d670dcc01a4ac1480
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-19 14:23:09 +0100

    pages & routes

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Adipisicing voluptate consequat reprehenderit magna deserunt. Dolore
+        amet deserunt quis nisi. Qui aliquip non aliqua nisi sit. Eiusmod magna
+        commodo laborum ad et eiusmod occaecat eu incididunt dolor.
+      </p>
+    </div>
+  );
+};
+
+export default About;
