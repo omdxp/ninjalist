6c559bd3fb35647eb244cebcc97fada88cd43afd
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-19 15:08:03 +0100

    redirecting user

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,15 @@
 import Link from "next/link";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 const NotFound = () => {
+  const router = useRouter();
+  useEffect(() => {
+    document.title = "404 - Page Not Found";
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  }, []);
   return (
     <div className="not-found">
       <h1>Ooooops...</h1>
