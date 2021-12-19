5d9678ab70ff9a9302eff2b2392ab036b35bf5f4
Omar-Belghaouti <omarbelghaouti@gmail.com>
2021-12-19 15:33:46 +0100

    fetching data

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,6 +1,17 @@
 import Head from "next/head";
+import styles from "../../styles/Ninjas.module.css";
 
-const Ninjas = () => {
+export const getStaticProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const json = await response.json();
+  return {
+    props: {
+      ninjas: json,
+    },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
   return (
     <>
       <Head>
@@ -9,6 +20,13 @@ const Ninjas = () => {
       </Head>
       <div>
         <h1>All Ninjas</h1>
+        {ninjas.map((ninja) => (
+          <div key={ninja.id}>
+            <a className={styles.single}>
+              <h3>{ninja.name}</h3>
+            </a>
+          </div>
+        ))}
       </div>
     </>
   );
